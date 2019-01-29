105188de3f5364c2a57d95cca8fac3a4d0db452d
GirlSuzette <trinitysuzette_marpis@hotmail.com>
2019-01-29 11:45:07 -0600

    mongoose

---
+++ src/routes/api.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const app = Router();
+
+// const Jobs = require('../controllers/')
+
+
+
+//Jobs routes
+// app.get('/jobs', Jobs.index);
+
+
+
+module.exports = app;
